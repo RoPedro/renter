6e36b8bbff55894c37b2d06308d638ec6141dc74
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-08-05 17:17:43 -0300

    Adding car creation

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,4 +6,25 @@ class CarsController < ApplicationController
   def show
     @car = Car.includes(:manufacturer).find(params[:id])
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to @car
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+    def car_params
+      params.require(:car).permit(:name, :manufacturer_id, :engine, :transmission, :horsepower, :released_at)
+    end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :manufacturer
+
+  validates :name, presence:true
+  validates :manufacturer, presence: true
 end
