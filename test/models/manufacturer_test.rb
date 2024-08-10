7e69d68441772df68d22764c90e716f0771adc74
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-08-10 14:14:42 -0300

    Adding first tests

--- test/models/manufacturer_test.rb
+++ test/models/manufacturer_test.rb
@@ -4,4 +4,8 @@ class ManufacturerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "valid manufacturer" do
+    manufacturer = Manufacturer.new(name: "Plymouth", email: "plymouth@support.com")
+    assert manufacturer.valid?
+  end
 end
