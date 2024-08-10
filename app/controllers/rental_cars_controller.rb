6251aae97043575fb64781856fc78e1d8226c0fb
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-08-10 18:05:30 -0300

    Creating dummy view for rental_cars

--- app/controllers/rental_cars_controller.rb
+++ app/controllers/rental_cars_controller.rb
@@ -2,4 +2,23 @@ class RentalCarsController < ApplicationController
   def index
     @rental_cars = RentalCar.all
   end
+
+  def new
+    @rental_car = RentalCar.new
+  end
+
+  def create
+    @rental_car = RentalCar.new(rental_car_params)
+
+    if @rental_car.save
+      redirect_to @rental_car
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def rental_car_params
+      params.require(:rental_car).permit(:license_plate, :car_id)
+    end
 end
--- app/models/rental_car.rb
+++ app/models/rental_car.rb
@@ -1,3 +1,5 @@
 class RentalCar < ApplicationRecord
   belongs_to :car
+
+  validates :license_plate, presence: true
 end
