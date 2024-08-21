fbc7a000001a688b931ed3bc21c72a8c4313e6ed
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-08-21 15:56:30 -0300

    Adding more simple tests

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @dummyCar1 = Car.create(name: "Car1") 
+  end
+
+  test "should not save car without a name" do
+    car = Car.new(name: nil)
+    assert_not car.save, "Saved the car without a name"
+  end
+
+  test "should not save car with the same name" do
+    dummyCar2 = Car.new(name: @dummyCar1.name) 
+    assert_not dummyCar2.save, "Saved the car with the same name"
+  end
 end
+
+# Notes: assert_not is the same as saying that this should not happen
