d2cde51baafefe26f0e7941edbae8e342f165134
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-08-22 09:19:47 -0300

    Adding client model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_10_152609) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_22_010422) do
   create_table "cars", force: :cascade do |t|
     t.string "name"
     t.string "engine"
@@ -22,6 +22,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_152609) do
     t.datetime "updated_at", null: false
     t.boolean "is_archived", default: false
     t.index ["manufacturer_id"], name: "index_cars_on_manufacturer_id"
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "manufacturers", force: :cascade do |t|
