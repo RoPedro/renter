9a2dcea48a9185d18d50e951e4647ada426e65fa
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-08-30 13:05:49 -0300

    Fixing failure messages for manufacturers tests

--- test/models/manufacturer_test.rb
+++ test/models/manufacturer_test.rb
@@ -7,12 +7,12 @@ class ManufacturerTest < ActiveSupport::TestCase
 
   test "should not save manufacturer without a name" do
     dummyManufacturer2 = Manufacturer.new(name: nil)
-    assert_not dummyManufacturer2.save
+    assert_not dummyManufacturer2.save, "Saved manufacturer without a name"
   end
 
   test "should not save manufacturer with the same name" do
     dummyManufacturer3 = Car.new(name: @manufacturer.name) 
-    assert_not dummyManufacturer3.save, "Saved the car with the same name"
+    assert_not dummyManufacturer3.save, "Saved manufacturer with the same name"
   end
 
   test "valid manufacturer" do
