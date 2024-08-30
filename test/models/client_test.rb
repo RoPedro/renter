9ee1ffe8edc3af8b1e6281771c9f1bdd6c4c4fc4
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-08-30 12:57:52 -0300

    Adding basic tests and fixtures for clients
    Adding validation to clients

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @client = clients(:one)
+  end
+
+  test "should not save client without a first name" do
+    dummyClient = Client.new(first_name: nil)
+    assert_not dummyClient.save, "Saved the client without a first name"
+  end
+
+  test "should not save client without a last name" do
+    dummyClient = Client.new(last_name: nil)
+    assert_not dummyClient.save, "Saved the client without a last name"
+  end
+
+  test "should not save client with duplicate email" do
+    duplicateClient = Client.new(email: @client.email) 
+    assert_not duplicateClient.save, "Saved the client with the same email"
+  end
 end
