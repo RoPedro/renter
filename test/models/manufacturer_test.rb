2d24b363c06597fcc668d7615ee700bcf82321ec
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-08-31 11:57:45 -0300

    Code quality on manufacturer model test

--- test/models/manufacturer_test.rb
+++ test/models/manufacturer_test.rb
@@ -6,13 +6,13 @@ class ManufacturerTest < ActiveSupport::TestCase
   end
 
   test "should not save manufacturer without a name" do
-    dummyManufacturer2 = Manufacturer.new(name: nil)
-    assert_not dummyManufacturer2.save, "Saved manufacturer without a name"
+    dummyManufacturer = Manufacturer.new(name: nil)
+    assert_not dummyManufacturer.save, "Saved manufacturer without a name"
   end
 
   test "should not save manufacturer with the same name" do
-    dummyManufacturer3 = Car.new(name: @manufacturer.name) 
-    assert_not dummyManufacturer3.save, "Saved manufacturer with the same name"
+    dummyManufacturer = Car.new(name: @manufacturer.name) 
+    assert_not dummyManufacturer.save, "Saved manufacturer with the same name"
   end
 
   test "valid manufacturer" do
