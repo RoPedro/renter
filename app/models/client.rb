8aa0c299087a5c72adf8c6731627b3ae3643bb1e
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-09-05 14:12:14 -0300

    feature: client model now validates to use only letters

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,4 @@
 class Client < ApplicationRecord
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, presence: true, :format => { with: /\A[a-zA-Z]+\z/, :message => 'Only letters allowed.' }
   validates :email, presence: true, uniqueness: true
 end
