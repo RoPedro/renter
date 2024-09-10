a2d138370eaf589378661f07d6a24fa240d9df53
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-09-10 10:37:51 -0300

    Adding status validation to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :client
   belongs_to :rental_car
+  validate :rental_must_be_available
+
+  enum status: { pending: 'pending', returned: 'returned' }
+
+  private
+
+  def rental_must_be_available
+    unless rental_car.status == 'available'
+      errors.add(:rental_car, "must be available to save")
+    end
+  end
 end
