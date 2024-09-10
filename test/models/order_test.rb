106a97af05651af9f724999eab2ea703e6c36efc
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-09-10 10:38:20 -0300

    Adding basic tests for orders model

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @valid_order = orders(:one)
+    @unavailable_order = orders(:three)
+  end
+
+  test 'shouold save valid order' do
+    assert @valid_order.save, 'order not saved even though was valid'
+  end
+  test 'should not save order if rental is unavailable' do
+    assert_not @unavailable_order.save, 'order saved even though rental was unavailable'
+    assert_includes @unavailable_order.errors[:rental_car], "must be available to save"
+  end
+
 end
