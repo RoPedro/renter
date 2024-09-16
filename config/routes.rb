ed83bd69034f37c43d0e6371b5c5e11097e4dd30
Pedro Moreira <pedro.lucas.rocha72@gmail.com>
2024-09-16 16:18:51 -0300

    feat: adding route resources for orders

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   resources :cars
   resources :rental_cars
   resources :clients
+  resources :orders do
+    member do
+      post :return
+    end
+  end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
